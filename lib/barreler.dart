8782f72af7f69d864c49d2f95b23c75ebc97bf72
Morgan <mrgnhnt96@gmail.com>
2024-03-06 15:21:17 -0700

    feat: create barrel file

---
+++ lib/barreler.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+export 'barreler_runner.dart';
+export 'commands/build_command.dart';
+export 'commands/example_command.dart';
+export 'handlers/build_handler.dart';
+export 'src/barrel.dart';
+export 'src/converter_utils/string_or_list.dart';
+export 'src/extensions/string_extension.dart';
+export 'src/find_settings.dart';
+export 'src/settings/default_settings.dart';
+export 'src/settings/directory_settings.dart';
+export 'src/settings/export_settings.dart';
+export 'src/settings/settings.dart';
+export 'src/version.dart';
